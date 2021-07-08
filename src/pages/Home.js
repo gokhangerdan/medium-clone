cce1e217009a9611a6f06e7dd80d99c9b1b3339a
gokhangerdan <gokhang1327@gmail.com>
2021-07-09 00:22:13 +0300

    discover view added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import { Button, Container, Grid } from "@material-ui/core";
 
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero.js';
-
 import Trending from "../components/Trending";
 
-
+import user from "../static/user.png"
+import image from "../static/image.jpg"
+import bookmark from "../static/bookmark.svg"
 
 
 function Home() {
@@ -15,6 +16,28 @@ function Home() {
         <Navigation />
         <Hero />
         <Trending />
+        <div>
+          <Container>
+            <div>
+              <div>
+                <img src={user} />
+                <h4>Dr. Derek Austin</h4>
+              </div>
+              <h2>Kent C. Dodd’s .filter() Trick Will Change How You Use JavaScript</h2>
+              <div>
+                <h3>This one-liner uses the Boolean constructor to magically remove all falsy values from an array ✨</h3>
+              </div>
+              <div>
+                <div>
+                <span>Jun 28 &nbsp;· &nbsp;3 min read &nbsp;· &nbsp;</span>
+                <span>JavaScript</span>
+                <img src={bookmark} />
+                </div>
+              </div>
+            </div>
+            <img src={image} />
+          </Container>
+        </div>
       </div>
     );
   }
