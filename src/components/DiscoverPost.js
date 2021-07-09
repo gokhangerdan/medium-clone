69b80da628e28dfcb54daaa78844ddcfd869394f
gokhangerdan <gokhang1327@gmail.com>
2021-07-09 03:09:46 +0300

    Discover posts styles added

---
+++ src/components/DiscoverPost.js
@@ -0,0 +1,33 @@
+import { Button, Container, Grid } from "@material-ui/core";
+
+import user from "../static/user.png"
+import image from "../static/image.jpg"
+import bookmark from "../static/bookmark.svg"
+
+function DiscoverPost(props) {
+    return (
+      <div>
+        <Container>
+          <div className="Discover_Post">
+            <div className="Post_Body">
+              <div className="Discover_User">
+                <img src={user} />
+                <span>Ella Alderson</span>
+              </div>
+              <h2>This Long-Awaited Technology May Finally Change the World</h2>
+              <h3>Solid-state batteries are poised to emerge in the coming years</h3>
+              <div>
+                <span className="Post_Info">May 30 &nbsp;· &nbsp;6 min read</span>
+                <span className="Post_Label_Seperator"> &nbsp;· &nbsp;</span>
+                <span className="Post_Label">Science</span>
+                <img  className="Post_Bookmark" src={bookmark} />
+              </div>
+            </div>
+            <img className="Post_Image" src={image} />
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  
+  export default DiscoverPost;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
 import { Button, Container, Grid } from "@material-ui/core";
 
-import Navigation from '../components/Navigation';
+import Navigation from '../components/Navigation.js';
 import Hero from '../components/Hero.js';
-import Trending from "../components/Trending";
-
-import user from "../static/user.png"
-import image from "../static/image.jpg"
-import bookmark from "../static/bookmark.svg"
+import Trending from "../components/Trending.js";
+import DiscoverPost from "../components/DiscoverPost.js"
 
 
 function Home() {
@@ -16,28 +13,7 @@ function Home() {
         <Navigation />
         <Hero />
         <Trending />
-        <div>
-          <Container>
-            <div>
-              <div>
-                <img src={user} />
-                <h4>Dr. Derek Austin</h4>
-              </div>
-              <h2>Kent C. Dodd’s .filter() Trick Will Change How You Use JavaScript</h2>
-              <div>
-                <h3>This one-liner uses the Boolean constructor to magically remove all falsy values from an array ✨</h3>
-              </div>
-              <div>
-                <div>
-                <span>Jun 28 &nbsp;· &nbsp;3 min read &nbsp;· &nbsp;</span>
-                <span>JavaScript</span>
-                <img src={bookmark} />
-                </div>
-              </div>
-            </div>
-            <img src={image} />
-          </Container>
-        </div>
+        <DiscoverPost />
       </div>
     );
   }
