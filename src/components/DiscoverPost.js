993919615411c8d40341bcf39a13bf313f60d8fd
gokhangerdan <gokhang1327@gmail.com>
2021-07-09 13:59:12 +0300

    discover categories added

--- src/components/DiscoverPost.js
+++ src/components/DiscoverPost.js
@@ -6,26 +6,22 @@ import bookmark from "../static/bookmark.svg"
 
 function DiscoverPost(props) {
     return (
-      <div>
-        <Container>
-          <div className="Discover_Post">
-            <div className="Post_Body">
-              <div className="Discover_User">
-                <img src={user} />
-                <span>Ella Alderson</span>
-              </div>
-              <h2>This Long-Awaited Technology May Finally Change the World</h2>
-              <h3>Solid-state batteries are poised to emerge in the coming years</h3>
-              <div>
-                <span className="Post_Info">May 30 &nbsp;· &nbsp;6 min read</span>
-                <span className="Post_Label_Seperator"> &nbsp;· &nbsp;</span>
-                <span className="Post_Label">Science</span>
-                <img  className="Post_Bookmark" src={bookmark} />
-              </div>
-            </div>
-            <img className="Post_Image" src={image} />
+      <div className="Discover_Post">
+        <div className="Post_Body">
+          <div className="Discover_User">
+            <img src={user} />
+            <span>Ella Alderson</span>
           </div>
-        </Container>
+          <h2>This Long-Awaited Technology May Finally Change the World</h2>
+          <h3>Solid-state batteries are poised to emerge in the coming years</h3>
+          <div>
+            <span className="Post_Info">May 30 &nbsp;· &nbsp;6 min read</span>
+            <span className="Post_Label_Seperator"> &nbsp;· &nbsp;</span>
+            <span className="Post_Label">Science</span>
+            <img  className="Post_Bookmark" src={bookmark} />
+          </div>
+        </div>
+        <img className="Post_Image" src={image} />
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,31 @@ function Home() {
         <Navigation />
         <Hero />
         <Trending />
-        <DiscoverPost />
+        <Container>
+          <Grid container>
+            <Grid item lg={7} md={7} sm={7} xs={12}>
+              <DiscoverPost />
+            </Grid>
+            <Grid item lg={5} md={5} sm={5} xs={12}>
+              <div className="Discover">
+                <div className="Discover_Headline">
+                  <p>DISCOVER MORE OF WHAT MATTERS TO YOU</p>
+                </div>
+                <div className="Discover_Categories">
+                  <p>Self</p>
+                  <p>Relationships</p>
+                  <p>Data Science</p>
+                  <p>Programming</p>
+                  <p>Productivity</p>
+                  <p>Javascript</p>
+                  <p>Machine Learning</p>
+                  <p>Politics</p>
+                  <p>Health</p>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     );
   }
